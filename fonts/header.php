acc1d96c32e02028c7273529ddea6bab66a0b56c
aljoharah21 <aljoharah.ali.ruh@tuwaiq.edu.sa>
2021-10-09 01:37:42 +0300

    main

---
+++ fonts/header.php
@@ -0,0 +1,170 @@
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href=".css">
+    <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.dataTables.min.js"></script>
+  <script src="js/dataTables.bootstrap4.min.js"></script>
+  
+<script src="js/مكتبة22.js"></script>
+  
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+
+<style>
+ .a{ 
+ font-size:1.4em;
+ border:2px solid #ffebcd;
+
+ }
+ul li{
+		
+nav{
+	width:100%;
+	background-color:#007bff;
+	z-index:5;
+	
+}
+} .nav-link{
+	text-align:right;
+ font-size:1.4em;
+ border:0px solid #ffebcd;	
+}
+ @media(max-width:700px){
+#navbar ul li:hover{
+	background-color:red;
+
+
+} 
+#imm{
+	width:20%;
+	
+}
+nav{
+	width:100%;
+	position:fixed;
+	
+}
+#hh{
+	
+	font-size:1.4em;
+}
+#main{
+height:200px;	
+}
+.imgg{
+	display:none;
+ } 
+	
+nav{
+	
+	background-color:#007bff;
+	z-index:5;
+	
+}
+.nav-item{
+ border:0px solid #ffebcd;
+	
+}
+ .nav-link{ 
+ font-size:1.4em;
+ border:0px solid #ffebcd;
+ }
+ label{
+ 
+ text-align:center;
+ }
+
+ ul li a{
+
+	 align:center;
+	 
+ }
+ #navbar ul li:hover{
+	background-color:red;
+}
+
+ }
+.nav-link{
+	 color:white;
+	 text-align:center;
+	 background-color:#007bff;
+width:100%;	 
+ }
+
+</style>
+</head>
+<body   dir='rtl' style=" background-color:#add8e6; background-image:url('img//img2.jpg' );">
+<div  id='main' style='position:fixed;top:0;center:0; width:100%; background-color:#e0dfdd;z-index:1;height:200px; '>
+            <div class=" text-center">
+           
+           	<!--<img  class="div-address-img5"src="img//img0.png"/>-->
+           
+            <div class="col-md-2">
+			
+     		<img width='90%' id='imm'  class="div-address-img2" src="img//C2.png"/>
+     	 </div>
+		    <div id="" class="col-md-8">
+			<h1 id='hh'style='color:red; fon-family:normal;font-size:3em;' > جامعة البيضاء
+<br>
+<small style='color:red; '>ALBYDA UNIVERSTY</small></h1> 
+		    </div>
+			
+	    	<div class="col-md-2"   >
+			<div  class='imgg' >
+			<img  width='90%' class="div-address-img6" src="img//c2.png"/>
+	    	</div>
+	    	</div>
+            <!--  --> 
+		    
+		    </div>
+		    
+<!--	    <img src='img/header.jpg' />-->
+
+
+            <div class="col-lg-12">
+<nav class="navbar navbar-default" style='height:55px; width:100%;background-color:#007bff; position:fixed;left:0;'>
+<div class="container">
+	     <div  class="navbar-header"  style='width:100%;' >
+	
+        <button style='background-color:white' class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+       <span  class="icon-bar"></span>
+        <span class="icon-bar"></span>
+  
+          </button>
+</div>
+
+
+<div   id="navbar" class="navbar-collapse collapse " >
+
+	  
+<ul id='n_nav' dir="rtl"  style="  font-size:1.5em;"  class="nav navbar-nav navbar-right" >
+      <li class="nav-item">
+        <a class="nav-link" style='color:white; text-align:center;' href="logout.php">Logout</a>
+      </li>   
+      <li class="nav-item">
+        <a class="nav-link" style='color:white; text-align:center; '  href="teacher.php">الاتصال بنا             </a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" style='color:white; text-align:center; ' href="page.php">استعراض النتائج</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style='color:white; text-align:center;' target='_blank' href="دليل_الطالب_الجامعي_للعام_2021_2022م_النهائي_والاخير_مع_الغلاف.pdf">دليل الطالب الجامعي</a>
+      </li>
+ 
+<li class="nav-item"><a class="nav-link" style='color:white; text-align:center; ' href="grade.php">نبذه عامه</a>
+	  </li>
+</ul>
+
+</div>
+
+</nav>
+</div>
+</div>
+</div>
+
